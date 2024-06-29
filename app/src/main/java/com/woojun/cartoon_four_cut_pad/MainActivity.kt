ee25295bf1775a3fb05b7ecde615a9923b76a0ad
JangWoojun <woojun0107@naver.com>
2024-06-30 05:03:19 +0900

    feat: takePhotoButton 클릭 이벤트 추가
    SelectModeActivity 이동

--- app/src/main/java/com/woojun/cartoon_four_cut_pad/MainActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut_pad/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.woojun.cartoon_four_cut_pad
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.woojun.cartoon_four_cut_pad.databinding.ActivityMainBinding
+import com.woojun.cartoon_four_cut_pad.util.OnSingleClickListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.takePhotoButton.setOnClickListener(object : OnSingleClickListener() {
+            override fun onSingleClick(v: View?) {
+                startActivity(Intent(this@MainActivity, SelectModeActivity::class.java))
+            }
+        })
+
+
     }
 }
