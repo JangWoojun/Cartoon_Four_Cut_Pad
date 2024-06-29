c10677ca91b970ab13c7300f198e71598db81894
JangWoojun <woojun0107@naver.com>
2024-06-30 04:47:34 +0900

    feat: SplashActivity 이동 로직 및 애니메이션 구현

--- app/src/main/java/com/woojun/cartoon_four_cut_pad/SplashActivity.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut_pad/SplashActivity.kt
@@ -1,20 +1,57 @@
 package com.woojun.cartoon_four_cut_pad
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.woojun.cartoon_four_cut_pad.databinding.ActivitySplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private lateinit var logoList: List<Pair<ObjectAnimator, Long>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        logoList = listOf(
+            Pair(createFadeInAnimation(binding.logo1), 300),
+            Pair(createFadeInAnimation(binding.logo2), 400),
+            Pair(createFadeInAnimation(binding.logo3), 500),
+            Pair(createFadeInAnimation(binding.logo4), 600)
+        )
+
+        CoroutineScope(Dispatchers.Main).launch {
+            animateLogos()
+            startMainActivity()
+        }
+
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finishAffinity()
+    }
+
+    private fun createFadeInAnimation(target: ImageView): ObjectAnimator {
+        val fadeIn = ObjectAnimator.ofFloat(target, "alpha", 0f, 1f)
+        fadeIn.duration = 200
+        return fadeIn
+    }
+
+    private suspend fun animateLogos() {
+        withContext(Dispatchers.Main) {
+            logoList.forEach { (animation, delay) ->
+                delay(delay)
+                animation.start()
+            }
+            delay(200)
         }
     }
 }
