74620c56fd1c29e26027276d39bb5d119f5c34a4
JangWoojun <woojun0107@naver.com>
2024-07-01 14:43:10 +0900

    feat: putImage 요청 추가

--- app/src/main/java/com/woojun/cartoon_four_cut_pad/network/RetrofitAPI.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut_pad/network/RetrofitAPI.kt
@@ -3,9 +3,14 @@ package com.woojun.cartoon_four_cut_pad.network
 import com.woojun.cartoon_four_cut_pad.BuildConfig
 import com.woojun.cartoon_four_cut_pad.data.Filter
 import com.woojun.cartoon_four_cut_pad.data.FrameResponse
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.PUT
+import retrofit2.http.Part
 
 
 interface RetrofitAPI {
@@ -15,4 +20,11 @@ interface RetrofitAPI {
     @GET("${BuildConfig.baseUrl}filter?image=true")
     fun getFilter(): Call<List<Filter>>
 
+    @Multipart
+    @PUT("${BuildConfig.baseUrl}image")
+    fun putImage(
+        @Header("auth") auth: String,
+        @Header("type") type: String,
+        @Part body: List<MultipartBody.Part>,
+    ): Call<List<String>>
 }
