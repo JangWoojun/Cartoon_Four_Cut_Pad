c5554643831ad1f99bdcd64883c9100737a346fd
JangWoojun <woojun0107@naver.com>
2024-07-09 08:28:46 +0900

    feat: 레트로핏 Instance 개체 ScalarsConverterFactory 추가

--- app/src/main/java/com/woojun/cartoon_four_cut_pad/network/RetrofitClient.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut_pad/network/RetrofitClient.kt
@@ -5,6 +5,7 @@ import com.woojun.cartoon_four_cut_pad.BuildConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
@@ -22,6 +23,7 @@ object RetrofitClient {
             instance = Retrofit.Builder()
                 .baseUrl(BuildConfig.baseUrl)
                 .client(okHttpClient)
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
         }
