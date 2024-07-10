f0739b2bd0b5c5b19a2ceaf7a2b0a68d24837d7b
JangWoojun <woojun0107@naver.com>
2024-07-10 18:26:16 +0900

    feat: Frame DataClass 속성 수정
    중복되어 불필요한 images 속성을 제거

--- app/src/main/java/com/woojun/cartoon_four_cut_pad/data/Frame.kt
+++ app/src/main/java/com/woojun/cartoon_four_cut_pad/data/Frame.kt
@@ -1,9 +1,7 @@
 package com.woojun.cartoon_four_cut_pad.data
 
-import android.graphics.Bitmap
 
 data class Frame (
     val name: String,
     val frameResponse: FrameResponse,
-    val images: MutableList<Bitmap>,
 )
